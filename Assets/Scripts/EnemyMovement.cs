0ddae0a4ed4335e3918464a350898f056ea823c9
Guilherme Mangerona Anicio <gui.anicio@gmail.com>
2023-12-12 17:58:20 -0300

    NavMesh funcionando e inicio do código de ronda dos inimigos

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class EnemyMovement : MonoBehaviour
 {
 
-    [SerializeField] Transform target;
     public NavMeshAgent agent;
+
+    public bool rondando = true;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +22,22 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(rondando)
+        {
+            rondar();
+        }
+
         agent.SetDestination(target.position);
+    }
+
+    void rondar()
+    {
+        GameObject pontosDeRonda = GameObject.FindGameObjectWithTag("PontosDeRonda");
+        int numPontosDeRonda = pontosDeRonda.transform.childCount;
+
+        int rand = Random.Range(0, numPontosDeRonda);
+        Transform pontoSelecionado = pontosDeRonda.transform.GetChild(rand);
+        agent.SetDestination(pontoSelecionado.position);
+
     }
 }
